61c6a91fca4c6a0171df1e22814c46dd22f95f45
Tuan Dinh <leonacky@gmail.com>
2016-11-28 12:04:23 +0700

    init google login ios

---
+++ ios/RNGoogleLogin/RNGoogleLogin/RNGoogleLogin.h
@@ -0,0 +1,13 @@
+//
+//  RNGoogleLogin.h
+//  RNGoogleLogin
+//
+//  Created by Tuan Dinh on 11/28/16.
+//  Copyright © 2016 Aotasoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNGoogleLogin : NSObject
+
+@end
---
+++ ios/RNGoogleLogin/RNGoogleLogin/RNGoogleLogin.m
@@ -0,0 +1,13 @@
+//
+//  RNGoogleLogin.m
+//  RNGoogleLogin
+//
+//  Created by Tuan Dinh on 11/28/16.
+//  Copyright © 2016 Aotasoft. All rights reserved.
+//
+
+#import "RNGoogleLogin.h"
+
+@implementation RNGoogleLogin
+
+@end
